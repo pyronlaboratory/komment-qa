90c51a0bf6963768c608ab6f2cbf9acbbba9ad73
Ronnie <ronnie94official@gmail.com>
2024-02-19 13:55:44 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,4 @@
-/**
- * @description This is an empty function that simply logs the string "0" to the
- * console once and then finishes executing.
- */
+
 const func0 = () => { 
   console.log("0"); 
   exit;
