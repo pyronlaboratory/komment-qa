5767090fefce82e0d0c9663474b58e646e9164b6
Ronnie <ronnie94official@gmail.com>
2024-02-19 14:14:05 +0000

    Update 🐛.js

--- 🐛.js
+++ 🐛.js
@@ -1,1 +1,11 @@
-🐛
+
+const search = (arr, x, start, end) => {
+  if (start > end) return false;
+  let mid = Math.floor((start + end) / 2);
+  if (arr[mid] === x) return true;
+  if (arr[mid] > x) {
+    return search(arr, x, start, mid - 1);
+  } else {
+    return search(arr, x, mid + 1, end);
+  }
+};
