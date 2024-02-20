5715d9b67e90988bedf5e876f9fd41286188222a
Ronnie <ronnie94official@gmail.com>
2024-02-20 12:42:40 +0000

    Update failthis.py

--- failthis.py
+++ failthis.py
@@ -1,7 +1,2 @@
 def failthis():
- """
- Nothing; it does not perform any actions or have any effect when called since
- "pass" does nothing and does not return any value upon execution.
-
- """
  pass
