171fd2a7f720e99943968d708c7df457b9b9e81f
Ronnie <ronnie94official@gmail.com>
2024-02-20 12:19:17 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
 
-/**
- * @description Nothing. The arrow function does not return anything and does not
- * have a name. The console log statement within the function is not executed since
- * nothing is returned.
- */
+const funcn0 = () => { 
+  console.log("test"); 
+}
+
+const funcn0 = () => { 
+  console.log("test"); 
+}
+
 const funcn0 = () => { 
   console.log("test"); 
 }
