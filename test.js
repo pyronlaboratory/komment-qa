a5373bae4890ad22b0d040fbd6f6e8d951c2c02d
Ronnie <ronnie94official@gmail.com>
2024-02-20 16:03:14 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,8 @@
 
-/**
- * @description This function does nothing. The arrow function defined inside the
- * empty parentheses {} simply logs the string "test" to the console and then returns
- * undefined due to the lack of return statement within the body of the function.
- */
 const funcn0 = () => { 
   console.log("test"); 
 }
 
-
-/**
- * @description The code `(() => { console.log("test");})();` is a function expression
- * that outputs the string "test" to the console.
- */
 const funcn0 = () => { 
   console.log("test"); 
 }
