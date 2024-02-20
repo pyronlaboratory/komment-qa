6409dacf6cbc36a1d5e75604e4ab9267ac4f05ae
Ronnie <ronnie94official@gmail.com>
2024-02-20 16:03:21 +0000

    Update test.js

--- test.js
+++ test.js
@@ -7,9 +7,6 @@ const funcn0 = () => {
   console.log("test"); 
 }
 
-/**
- * @description The function logs the string "test" to the console.
- */
 const funcn0 = () => { 
   console.log("test"); 
 }
