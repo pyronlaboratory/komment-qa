3d551bd801a6185595233088af4bba34e0ee4afe
Ronnie <ronnie94official@gmail.com>
2024-02-20 11:56:29 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,4 @@
-/**
- * @description This function does nothing; it is a function with no body and will
- * always return undefined since it has no returning statement.
- */
+
 const funcn0 = () => { 
   console.log("test");
 }
