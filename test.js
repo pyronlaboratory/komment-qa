9d57bd7960330236580b8bfd7169ccb13a7c6a72
Ronnie <ronnie94official@gmail.com>
2024-02-20 12:03:57 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,4 @@
 
-
-/**
- * @description Nothing; the code within the arrow function is never executed because
- * it is contained within a parentheses-less arrow function declaration and therefore
- * represents a function definition that is immediately evaluated as `undefined`.
- */
 const funcn0 = () => { 
-  console.log("test");
-  
+  console.log("test"); 
 }
-
