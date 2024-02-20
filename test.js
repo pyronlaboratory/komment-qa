b8087fd98ee7c443a32386205efb9a32b0a6738d
Ronnie <ronnie94official@gmail.com>
2024-02-20 15:00:38 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,4 @@
 
-/**
- * @description The code(() => { console.log("test"); }) is an immediate invoked
- * function expression (IIFE) that executes the function body only once when it's
- * called (i.e., logs "test" to the console).
- */
 const funcn0 = () => { 
   console.log("test"); 
 }
