af84a452af3fc8278df3de16de965276de66c1c5
Ronnie <ronnie94official@gmail.com>
2024-02-20 15:40:35 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,4 @@
 
-/**
- * @description The code(() => {console.log("test")}); is known as an immediately
- * invoked function expression (IIFE). It logs the string "test" to the console.
- */
 const funcn0 = () => { 
   console.log("test"); 
 }
