4f6e1dfc2f30a3bff205e87fa7eceebb3b789f27
Ronnie <ronnie94official@gmail.com>
2024-02-20 14:58:23 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,9 @@
 
-/**
- * @description Nothing; it is an empty function with no code body and just returns
- * the anonymous function itself when called.
- */
 const funcn0 = () => { 
   console.log("test"); 
 }
 
-/**
- * @description Nothing. The code Inside the arrow function never actually runs because
- * there is no value passed to it and thus no function call occurs. This construct
- * simply defines a function that doesn't get called
- */
+
 const funcn0 = () => { 
   console.log("test"); 
 }
