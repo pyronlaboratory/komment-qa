c1c47bf02d76349b0c1bbdb5a0b25cefa9375b86
Ronnie <ronnie94official@gmail.com>
2024-02-20 15:52:54 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,4 @@
 
-/**
- * @description The arrow function expression `(() => { console.log("test"); })();`
- * logs the string "test" to the console.
- */
 const funcn0 = () => { 
   console.log("test"); 
 }
