2dc203e01f3f266da1d2217d01a14c57a78db618
Ronnie <ronnie94official@gmail.com>
2024-02-20 15:57:29 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,4 @@
 
-/**
- * @description This function does nothing; it is an empty function expression that
- * simply logs the string "test" to the console but doesn't return any value.
- */
 const funcn0 = () => { 
   console.log("test"); 
 }
