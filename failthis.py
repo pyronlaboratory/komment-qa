49b9a4813a7bbee2191ae2bb3f2dcff33d8c5bfc
Ronnie <ronnie94official@gmail.com>
2024-02-20 12:44:14 +0000

    Update failthis.py

--- failthis.py
+++ failthis.py
@@ -1,7 +1,2 @@
 def failthis():
- """
- The given function "failthis()" does nothing and only passes. In other words ,
- it is a no-op or a blank function that doesn't perform any operations.
-
- """
  pass
