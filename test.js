f4db8be66e51174633521cbe2e0a905163596442
Ronnie <ronnie94official@gmail.com>
2024-02-20 12:16:44 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,4 @@
 
-/**
- * @description Nothing. It is an empty function declaration that logs the string
- * "test" to the console and does nothing else.
- */
 const funcn0 = () => { 
   console.log("test"); 
 }
