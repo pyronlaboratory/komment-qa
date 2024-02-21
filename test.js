6388e14ac0ae4b8f2abc8499a94b7c4ac8107e8c
Ronnie <ronnie94official@gmail.com>
2024-02-21 21:57:02 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,4 @@
 
-
-/**
- * @description Nothing. It is an empty function expression that outputs "test" to
- * the console log but has no effect because it is not called anywhere.
- */
 const funcn0 = () => { 
   console.log("test"); 
 }
